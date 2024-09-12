2f9b349bb554cad8804dc1cc8b3f92e1afdfbc5b
s-devoe <chelseg14@gmail.com>
2024-09-12 14:15:53 +0100

    feat: update d-park

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,11 @@ export interface SlotI {
   status: SlotStatus;
 }
 
+export interface GatesI {
+  id: "Entrance" | "Exit";
+  status: SlotStatus;
+}
+
 function App() {
   const { data, subscribe } = useFirebaseSubscription("parking");
 
@@ -46,8 +51,14 @@ function App() {
     { id: "D4", status: "free" },
   ]);
 
+  const [gates, setGates] = useState<GatesI[]>([
+    { id: "Entrance", status: "free" },
+    { id: "Exit", status: "free" },
+  ]);
+
   useEffect(() => {
     if (data) {
+      // slots
       setSlots((prevSlots) =>
         prevSlots.map((slot) => {
           let newStatus: SlotStatus = "free"; // Default status
@@ -62,12 +73,27 @@ function App() {
           return { ...slot, status: newStatus };
         })
       );
+      // gates
+      setGates((prevGates) =>
+        prevGates.map((gate) => {
+          let newStatus: SlotStatus = "free"; // Default status
+
+          // Map Firebase data to gate IDs
+          if (gate.id === "Entrance" && data.Entrance) {
+            newStatus = data.Entrance.status;
+          } else if (gate.id === "Exit" && data.Exit) {
+            newStatus = data.Exit.status;
+          }
+
+          return { ...gate, status: newStatus };
+        })
+      );
     }
   }, [data]);
 
   return (
-    <section className="text-black grid place-content-center">
-      <ParkingSlots slots={slots} />
+    <section className="text-black flex items-center md:justify-center pl-4 py-4 md:p-8">
+      <ParkingSlots slots={slots} gates={gates} />
     </section>
   );
 }
--- src/page-content/ParkingSlots.tsx
+++ src/page-content/ParkingSlots.tsx
@@ -1,26 +1,20 @@
-import { SlotI } from "../App";
+import { GatesI, SlotI } from "../App";
 import { CarSvg } from "../components/svg/CarSvg";
+import { cn } from "../lib/utils";
 import { SlotStatus } from "../type";
 
 interface Props {
   slots: SlotI[];
+  gates: GatesI[];
 }
 
-const ParkingSlots = ({ slots }: Props) => {
+const ParkingSlots = ({ slots, gates }: Props) => {
   return (
     <div className="relative h-fit w-fit p-12 border-2 border-black gap-20 flex flex-col items-center">
       {/* entry */}
-      <div className="absolute right-[-0.5rem] top-[8rem] flex items-center justify-center w-5 h-20 bg-white text-black font-bold text-lg">
-        <span className="rotate-90 transform font-medium tracking-wide text-sm ">
-          Entry
-        </span>
-      </div>
+      <Entrance status={gates[0].status} />
       {/* exit  */}
-      <div className="absolute right-[-0.5rem] bottom-[8rem] flex items-center justify-center w-5 h-20 bg-white text-black font-bold text-lg">
-        <span className="-rotate-90 transform font-medium tracking-wide text-sm ">
-          Exit
-        </span>
-      </div>
+      <Exit status={gates[1].status} />
       <div className="flex items-center">
         {slots.slice(0, 4).map((slot) => (
           <div
@@ -87,3 +81,62 @@ const ParkingStatus = ({ status }: { status: SlotStatus }) => {
     </span>
   );
 };
+
+function Entrance({ status }: { status: SlotStatus }) {
+  return (
+    <div
+      className={cn(
+        "absolute right-[-0.5rem] transition-all duration-1000 top-[8rem] flex items-center justify-center w-5 h-20 bg-white text-black font-bold text-lg",
+        {
+          "bg-yellow-500": status === "obstruction",
+          "bg-green-500": status === "occupied",
+          "bg-red-500 text-white": status === "free",
+        }
+      )}
+    >
+      <span className="rotate-90 transform font-medium tracking-wide text-sm ">
+        Entry
+      </span>
+      <CarSvg
+        className={cn(
+          "transition-all duration-1000 car_size z-[20] absolute -rotate-90",
+          {
+            "-mr-[6rem]": status === "obstruction",
+            "mr-0": status === "occupied",
+            invisible: status === "free",
+          }
+        )}
+      />
+    </div>
+  );
+}
+
+function Exit({ status }: { status: SlotStatus }) {
+  return (
+    <div
+      className={cn(
+        "absolute right-[-0.5rem] bottom-[8rem] transition-all duration-1000 flex items-center justify-center w-5 h-20 bg-white text-black font-bold text-lg",
+        {
+          "bg-yellow-500": status === "obstruction",
+          "bg-green-500": status === "occupied",
+          "bg-red-500 text-white": status === "free",
+        }
+      )}
+    >
+      <span className="-rotate-90 transform font-medium tracking-wide text-sm ">
+        Exit
+      </span>
+
+      <CarSvg
+        className={cn(
+          "transition-all duration-1000 car_size z-[20] absolute rotate-90",
+          {
+            "-ml-[8rem]": status === "obstruction",
+            "ml-0": status === "occupied",
+            " invisible": status === "free",
+          }
+        )}
+      />
+    </div>
+  );
+}
